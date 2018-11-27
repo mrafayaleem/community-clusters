14cbbc4efcaaeb822b1b1462c7e8648ad4dd5559
Nandita Dwivedi <nanditadwivedi@Nanditas-MacBook-Pro.local>
2018-11-27 02:33:33 -0800

    S3 boto warc download

--- bootstrap/parent_child.py
+++ bootstrap/parent_child.py
@@ -1,4 +1,4 @@
-import sys
+import sys,re
 import os
 
 from pyspark import SparkContext, SparkConf
@@ -6,10 +6,15 @@ from pyspark.sql import SQLContext
 from pyspark.sql.types import StructField, StringType, StructType
 
 from warcio import ArchiveIterator
+from warcio.recordloader import ArchiveLoadFailed
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import tldextract
 
+import boto3
+import botocore
+from io import BytesIO
+from tempfile import TemporaryFile
 
 schema = StructType([
     StructField('parent', StringType()),
@@ -23,25 +28,50 @@ def process_warcs(i_, iterator):
     # Currently, this function is processing from files in parallel across partitions.
     # We can extend this same function easily for S3
 
+    s3pattern = re.compile('^s3://([^/]+)/(.+)')
     base_dir = os.path.abspath(os.path.dirname(__file__))
 
+    no_sign_request = botocore.client.Config(
+        signature_version=botocore.UNSIGNED)
+    s3client = boto3.client('s3', config=no_sign_request)
+
     for uri in iterator:
-        if uri.startswith('file:'):
+        if uri.startswith('s3://'):
+            s3match = s3pattern.match(uri)
+            bucketname = s3match.group(1)
+            path = s3match.group(2)
+            warctemp = TemporaryFile(mode='w+b')
+            
+            try:
+                s3client.download_fileobj(bucketname, path, warctemp)
+            except botocore.client.ClientError as exception:
+                print('Failed to download from s3')
+                warctemp.close()
+                continue
+            warctemp.seek(0)
+            stream = warctemp
+
+        elif uri.startswith('file:'):
             uri = uri[5:]
-        uri = os.path.join(base_dir, uri)
+            uri = os.path.join(base_dir, uri)
+            try:
+                stream = open(uri, 'rb')
+            except IOError as exception:
+                print("Failed to read data from local")
+                continue
+        else:
+            print("Unknowm file system")
 
         try:
-            stream = open(uri, 'rb')
-        except IOError as exception:
-            continue
-
-        for record in ArchiveIterator(stream):
-            processed = process_record(record)
-
-            if processed:
-                yield processed
-            continue
-
+            for record in ArchiveIterator(stream):
+                processed = process_record(record)
+                if processed:
+                    yield processed
+                continue
+        except ArchiveLoadFailed as exception:
+            print('Invalid WARC')
+        finally:
+            stream.close()
 
 def process_record(record):
     if record.rec_type == 'response' and record.http_headers.get_header('Content-Type') == 'text/html':
@@ -99,9 +129,16 @@ def get_external_links(html_content, parentTLD, parent):
     return link_list
 
 
-def main(input_file, output_file):
+def main(input_file, output_file, file_system, to_crawl_data):
     input_data = sc.textFile(input_file)
     print('INDATA', input_data.collect())
+
+    if(file_system=="s3"):
+        input_data = input_data.map(lambda p: "s3://" + to_crawl_data + "/" + p)
+    elif(file_system=="file"):
+        input_data = input_data.map(lambda p: "file:" + to_crawl_data + "/" + p)  
+    else:
+        print("file system not found.")
 
     partition_mapped = input_data.mapPartitionsWithIndex(process_warcs)
     mapped = partition_mapped.flatMap(lambda x: x)
@@ -124,5 +161,7 @@ if __name__ == '__main__':
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+    file_system = sys.argv[3]
+    to_crawl_data = sys.argv[4]
 
-    main(input_file, output_file)
+    main(input_file, output_file, file_system, to_crawl_data)
