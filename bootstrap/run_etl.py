3dcbf90633cbdad2975f06bfdd165f0bfeb0801d
Rafay Aleem <mrafayaleem@gmail.com>
2018-11-30 17:23:11 -0800

    Add etl script

---
+++ bootstrap/run_etl.py
@@ -0,0 +1,29 @@
+import os
+
+
+THRESHOLD = 10
+TEMP_PATH = 'temp'
+
+
+def main():
+
+    with open('input/test_warc.txt') as fp:
+        data = fp.readlines()
+
+    tout = []
+    for i, line in enumerate(data):
+        tout.append(line)
+
+        if (i + 1) % THRESHOLD == 0:
+            with open(TEMP_PATH + '/t{0}'.format(i), 'w') as file_handler:
+                for item in tout:
+                    file_handler.write("{}\n".format(item))
+
+        elif i == len(data) - 1:
+            with open(TEMP_PATH + '/t{0}'.format(i), 'w') as file_handler:
+                for item in tout:
+                    file_handler.write("{}\n".format(item))
+
+
+if __name__ == "__main__":
+    main()
