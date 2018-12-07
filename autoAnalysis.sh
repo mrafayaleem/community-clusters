d6f9db7105869c828c65f0ad15f5d43a45655ebc
Seyi Talabi <oatalabi@gmail.com>
2018-12-07 14:04:27 -0800

    aray index

--- autoAnalysis.sh
+++ autoAnalysis.sh
@@ -9,7 +9,7 @@ echo "Running analysis.py"
 for file in data/ETLout/*; do
 	echo "reading file $file"
 	IFS='/ ' read -r -a array <<< $file
-	month=${array[3]}
+	month=${array[2]}
 	echo $month
 	${SPARK_HOME}/bin/spark-submit --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11 analysis.py --inputs $file --outputs $month --path data/AnalysisOut/$1 --focus $2
 done
