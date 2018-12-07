668022a94843cb57a9b1a05e1aa1a9ba1f13b94e
Nandita Dwivedi <nandita.17.11@gmail.com>
2018-12-07 01:02:48 -0800

    script works for analysis.py automation

--- autoAnalysis.sh
+++ autoAnalysis.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
   
-for file in data/ETLout/*; do
-    	#echo `spark-submit analysis.py --inputs $file/*/  --outputs AnalysisOut/ twitter google facebook $file`
+for file in data/ETLout/test/*; do
+	echo "reading file $file"
 	IFS='/ ' read -r -a array <<< $file
-	month=${array[2]}
-	echo "$month"	
-	echo "$file/*/"
+	month=${array[3]}
+	echo `spark-submit --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11 analysis.py --inputs $file/*/  --outputs $month --path data/AnalysisOut/$1 --focus amazon`
 done
 
 #echo "id,name,label,pagerank" | cat - tnew
