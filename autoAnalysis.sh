75134085021adb4a27f568138c93f14847fb341a
Nandita Dwivedi <nandita.17.11@gmail.com>
2018-12-07 16:51:09 -0800

    all analysis automated

--- autoAnalysis.sh
+++ autoAnalysis.sh
@@ -1,50 +1,30 @@
 #!/usr/bin/env bash
 
 #$1:shopping
-#$2:amazon ebay
-#$3:month to run plot_communities on
+#$2:'amazon ebay'
+#$3:may
   
 echo "Running analysis.py"
 
-for file in data/ETLout/*; do
-	echo "reading file $file"
-	IFS='/ ' read -r -a array <<< $file
- 	month=${array[2]}
-	echo $month
-	${SPARK_HOME}/bin/spark-submit --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11 analysis.py --inputs $file --outputs $month --path data/AnalysisOut/$1 --focus $2
-	#echo `spark-submit --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11 analysis.py --inputs $file --outputs $month --path data/AnalysisOut/$1 --focus $2`
-	wait $!
-done
+month=$3
+category=$1
+${SPARK_HOME}/bin/spark-submit --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11 analysis.py --inputs data/ETLout/$month --outputs $month --path data/AnalysisOut/$1 --focus $2 
+#echo `spark-submit --packages graphframes:graphframes:0.6.0-spark2.3-s_2.11 analysis.py --inputs data/ETLout/test/$month --outputs $month --path data/AnalysisOut/$1 --focus $2`
+wait $!
 
 echo "Concatenating for D3"
 
-for file in data/AnalysisOut/$1/rankings-*; do
+echo `cat data/AnalysisOut/$category/rankings-$month/part-* > data/htmlInput/$month.csv`
+printf '%s\n' "id,name,label,pagerank" | cat - data/htmlInput/$month.csv > data/htmlInput/rankings-$month.csv
+rm data/htmlInput/$month.csv
 
-	IFS='/ ' read -r -a array1 <<< $file
-	IFS='- ' read -r -a array2 <<< ${array1[3]}
-	month=${array2[1]}
+mkdir -p data/D3Input/$category
+echo `cat data/AnalysisOut/$category/links-$month/part-* > data/D3Input/$category/links-$month.csv`
 
-	echo `cat $file/part-* > data/htmlInput/$month.csv`
-	printf '%s\n' "id,name,label,pagerank" | cat - data/htmlInput/$month.csv > data/htmlInput/rankings-$month.csv
-	rm data/htmlInput/$month.csv
-done
-
-mkdir -p data/D3Input/$1
-for file in data/AnalysisOut/$1/links-*; do 
-	IFS='/ ' read -r -a array1 <<< $file
-        IFS='- ' read -r -a array2 <<< ${array1[3]}
-        month=${array2[1]}
-	echo `cat $file/part-* > data/D3Input/$1/links-$month.csv`
-done
-
-for file in data/AnalysisOut/$1/communities-*; do
-	IFS='/ ' read -r -a array1 <<< $file
-        IFS='- ' read -r -a array2 <<< ${array1[3]}
-        month=${array2[1]}
-	echo `cat $file/part-* >  data/D3Input/$1/communities-$month.csv`
-done
+echo `cat data/AnalysisOut/$category/communities-$month/part-* >  data/D3Input/$category/communities-$month.csv`
 
 echo "Running plot_communities.py"
 
-echo `python3 plot_communities.py --outputs $3 --path $1`
+echo `python3 plot_communities.py --outputs $month --path $category`
 
+sed "s/%%MONTH%%/$month/; s/%%CATEGORY%%/$category/" index-template.html > public/index-$category-$month.html
